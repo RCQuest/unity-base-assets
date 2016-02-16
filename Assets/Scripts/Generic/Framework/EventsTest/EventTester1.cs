7f4461db4e252c2c88ad94555ae150682e81572b
Rachel <me@rcabot.com>
2016-02-16 23:41:07 +0000

    Added fonts, icon mockups, event system testing and color schemes.

---
+++ Assets/Scripts/Generic/Framework/EventsTest/EventTester1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventTester1 : MonoBehaviour {
+    
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Events.instance.Raise(new EventTester2.Event1());
+    }
+}
---
+++ Assets/Scripts/Generic/Framework/EventsTest/EventTester2.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventTester2 : MonoBehaviour
+{
+
+    public class Event1 : GameEvent
+    {
+
+    }
+
+    public class Event2 : GameEvent
+    {
+
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        Events.EventDelegate<Event1> action = (e) => Debug.Log("action 1 triggered.");
+        Events.EventDelegate<Event1> action2 = (e) => Debug.Log("action 2 triggered.");
+        Events.instance.AddListener<Event1>(action);
+        Events.instance.AddListener<Event1>(action2);
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
